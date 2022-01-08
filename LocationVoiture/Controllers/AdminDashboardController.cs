3af4541eb41ed252ef754eb19ebeb5cbdd4142bc
Mouad Sellak <mouaadsellak123@gmail.com>
2022-01-08 12:49:55 +0100

    mouad add comment

--- LocationVoiture/Controllers/AdminDashboardController.cs
+++ LocationVoiture/Controllers/AdminDashboardController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace LocationVoiture.Controllers
 {
+    //mouadBranch comment
     [Authorize(Roles = "Admin")] 
     public class AdminDashboardController : BaseController
     {
